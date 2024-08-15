9d5752227e45e2d45cccd7d2bd81fb1d66c4e281
Ahmed <ahmeddhalilovic@gmail.com>
2024-08-15 23:49:57 +0200

    Initial Commit

---
+++ NoteTakingApp/NoteTakingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingAppApp.swift
+//  NoteTakingApp
+//
+//  Created by Ahmed Halilovic on 15. 8. 2024..
+//
+
+import SwiftUI
+
+@main
+struct NoteTakingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
